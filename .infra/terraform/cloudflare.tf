37d68193bebfd2b687c06cd61d09f9385b4424c5
Yohan Giarelli <yohan@les-tilleuls.coop>
2026-06-15 23:26:04 +0200

    fix: Fixed IPv6

--- .infra/terraform/cloudflare.tf
+++ .infra/terraform/cloudflare.tf
@@ -5,7 +5,7 @@ resource "cloudflare_dns_record" "dns" {
   zone_id = "3fa2035c4239d02756471c8a0f51f247"
   name    = "meteoprint.cvvfcm.fr"
   type    = "AAAA"
-  content = scaleway_instance_ip.v6.address
+  content = "${scaleway_instance_ip.v6.address}1"
   ttl     = 1
   proxied = true
 }
