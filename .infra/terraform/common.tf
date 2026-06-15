42b9d7e041d2a4b76decd689bbd20130ecd9de67
Yohan Giarelli <yohan@les-tilleuls.coop>
2026-06-15 22:10:33 +0200

    fix: Fixed Scaleway terraform

--- .infra/terraform/common.tf
+++ .infra/terraform/common.tf
@@ -3,6 +3,7 @@ resource "scaleway_account_project" "meteoprint" {
 }
 
 provider "scaleway" {
-  zone   = "nl-ams-1"
-  region = "nl-ams"
+  zone            = "nl-ams-1"
+  region          = "nl-ams"
+  organization_id = "8258be04-f116-4f9a-a052-25b519c929b4"
 }
--- .infra/terraform/registry.tf
+++ .infra/terraform/registry.tf
@@ -1,6 +1,7 @@
 resource "scaleway_registry_namespace" "docker_registry" {
-  name   = "meteoprint-registry"
-  region = "fr-par"
+  name       = "meteoprint-registry"
+  region     = "fr-par"
+  project_id = scaleway_account_project.meteoprint.id
 
   is_public = false
 }
