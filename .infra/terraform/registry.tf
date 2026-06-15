c1b06c32aaf2e1a3df68141978d3c4575eb6a2db
Yohan Giarelli <yohan@les-tilleuls.coop>
2026-06-15 22:30:44 +0200

    fix: FIxed Docker registry auth

--- .infra/terraform/registry.tf
+++ .infra/terraform/registry.tf
@@ -5,3 +5,21 @@ resource "scaleway_registry_namespace" "docker_registry" {
 
   is_public = false
 }
+
+resource "scaleway_iam_application" "github_cd" {
+  name = "github-cd-meteoprint"
+}
+
+resource "scaleway_iam_api_key" "github_cd_key" {
+  application_id = scaleway_iam_application.github_cd.id
+}
+
+resource "scaleway_iam_policy" "github_cd_policy" {
+  name           = "github-cd-registry-policy"
+  application_id = scaleway_iam_application.github_cd.id
+
+  rule {
+    project_ids          = [scaleway_account_project.meteoprint.id]
+    permission_set_names = ["ContainerRegistryFullAccess"]
+  }
+}
