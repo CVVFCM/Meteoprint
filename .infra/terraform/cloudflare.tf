7fcdb0d779ee4b241161b9c963a9a3c4866263c9
Yohan Giarelli <yohan@les-tilleuls.coop>
2026-06-15 23:28:25 +0200

    fix: Added explicit IP on IPv6

--- .infra/terraform/cloudflare.tf
+++ .infra/terraform/cloudflare.tf
@@ -5,7 +5,7 @@ resource "cloudflare_dns_record" "dns" {
   zone_id = "3fa2035c4239d02756471c8a0f51f247"
   name    = "meteoprint.cvvfcm.fr"
   type    = "AAAA"
-  content = "${scaleway_instance_ip.v6.address}1"
+  content = scaleway_instance_server.front.public_ips[0].address
   ttl     = 1
   proxied = true
 }
