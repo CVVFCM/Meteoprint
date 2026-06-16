d8d92dfad0dc83214f04c1ae108763aeaefbbd29
Yohan Giarelli <yohan@les-tilleuls.coop>
2026-06-16 08:20:40 +0200

    fix: Added back instance in paris

--- .infra/terraform/common.tf
+++ .infra/terraform/common.tf
@@ -3,7 +3,7 @@ resource "scaleway_account_project" "meteoprint" {
 }
 
 provider "scaleway" {
-  zone            = "nl-ams-1"
-  region          = "nl-ams"
+  zone            = "fr-par-1"
+  region          = "fr-par"
   organization_id = "8258be04-f116-4f9a-a052-25b519c929b4"
 }
