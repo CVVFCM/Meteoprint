0ff304d3872624ef8775d2d39a0e75f2c8680111
Yohan Giarelli <yohan@les-tilleuls.coop>
2026-06-25 12:31:49 +0200

    fix: Fixed playwright tests

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace App\DataFixtures;
 
+use App\Bridge\Ffvoile\FFVClubId;
+use App\Entity\Spot;
+use App\Entity\SpotType;
+use App\ValueObject\Geo;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -11,8 +15,8 @@ final class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $cvvfcm = Spot::create('CVVFCM', 'cvvfcm', new Geo(49.87, 4.6), SpotType::FFV_CLUB, new FFVClubId('08002'), '08500');
+        $manager->persist($cvvfcm);
 
         $manager->flush();
     }
