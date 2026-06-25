057db9d64e77006350cf1d4cb7e1c01f1ec916f5
Yohan Giarelli <yohan@les-tilleuls.coop>
2026-06-25 12:11:11 +0200

    fix: Fixed Schema sync

--- migrations/Version20260625084500.php
+++ migrations/Version20260625084500.php
@@ -17,12 +17,10 @@ final class Version20260625084500 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql('ALTER TABLE spot ADD postcode VARCHAR(5) DEFAULT NULL');
-        $this->addSql('CREATE INDEX IDX_B9327A73A95D0DD ON spot (postcode)');
     }
 
     public function down(Schema $schema): void
     {
         $this->addSql('DROP INDEX IDX_B9327A73A95D0DD');
-        $this->addSql('ALTER TABLE spot DROP postcode');
     }
 }
